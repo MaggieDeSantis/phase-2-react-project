2edf3f5d915f4cc5cdf4c6941fbe7c45614ecb68
MaggieDeSantis <sheisntreal@gmail.com>
2023-06-30 05:27:30 -0400

    fixes code in componets files

--- src/components/RandomNasaImage.js
+++ src/components/RandomNasaImage.js
@@ -1,30 +1,41 @@
 import React, { useEffect, useState } from 'react';
 
-function RandomNASAImage() {
-  const [imageUrl, setImageUrl] = useState('');
+const RandomNASAImage = () => {
+  const [randomImage, setRandomImage] = useState(null);
 
   useEffect(() => {
-    const fetchRandomNASAImage = async () => {
-      try {
-        const API_KEY = 'REACT_APP_API_KEY';
-        const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
-
-        const response = await fetch(url);
-        const data = await response.json();
-        setImageUrl(data.url);
-      } catch (error) {
-        console.log('Error fetching NASA image:', error);
-      }
-    };
-
-    fetchRandomNASAImage();
+    fetchRandomImage();
   }, []);
+
+  const fetchRandomImage = async () => {
+    try {
+      const response = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}`
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setRandomImage(data);
+      } else {
+        console.error('Error:', response.status);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
-      <img id="banner-image" src={imageUrl} alt="NASA Banner" />
+      <h2>Random NASA Image</h2>
+      {randomImage && (
+        <div>
+          <img src={randomImage.url} alt={randomImage.title} />
+          <h3>{randomImage.title}</h3>
+          <p>{randomImage.date}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default RandomNASAImage;
